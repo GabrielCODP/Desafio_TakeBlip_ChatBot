49a26eadaca1f7003ae96afaafcaaeed7ce87475
Gabriel F <gabrielcruci@hotmail.com>
2022-08-14 22:28:41 -0300

    Primeira Versão API ChatBot

---
+++ Api/Controllers/GitHubDadosController.cs
@@ -0,0 +1,51 @@
+﻿using Api.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GitHubDadosController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IEnumerable<GitHubViewModel>> GetRepositoriesGithub()
+        {
+            try
+            {
+                var requestRepositorio = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/users/takenet/repos?sort=created&direction=asc");
+
+
+                //Servidor necessida de um User-Agent
+                requestRepositorio.Headers.Add("Accept", "application/vnd.github.v3+json");
+                requestRepositorio.Headers.Add("User-Agent", "GabrielCODP");
+
+                var clientHttp = new HttpClient();
+                HttpResponseMessage response = await clientHttp.SendAsync(requestRepositorio);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseStream = await response.Content.ReadAsStreamAsync();
+
+                    var list = await JsonSerializer.DeserializeAsync
+                        <IEnumerable<GitHubViewModel>>(responseStream);
+
+
+                    //Os dados já vem ordendado da menor data de criação "ASC".
+
+                    return list.Where(X => X.Language == "C#").Take(5);
+                }
+
+                return Enumerable.Empty<GitHubViewModel>();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ Api/ViewModel/GitHubViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Api.ViewModel
+{
+    public class GitHubViewModel
+    {
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public string CreatedAt { get; set; }
+
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+    }
+}
